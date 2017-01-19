ebebdf15d6c0c99c667053707607b4c2b9e50123
Pirokarus <olimjon.kosymoff@yandex.ru>
2017-01-19 14:51:11 +0300

    4

--- src/main/java/olim/test.java
+++ src/main/java/olim/test.java
@@ -1,6 +1,7 @@
 package olim;
 
-import pojo.PkAttributeTypesEntity;
+import pojo.*;
+import org.hibernate.Session;
 
 /**
  * Created by Олимджон on 17.01.2017.
@@ -8,8 +9,19 @@ import pojo.PkAttributeTypesEntity;
 public class test {
 
     public static void main(String[] args) {
+
+        Session session = HibernateSessionFactory.getSessionFactory().openSession();
+
+        session.beginTransaction();
         PkAttributeTypesEntity a1 = new PkAttributeTypesEntity();
         a1.setAttributeTypeId(1);
         a1.setName("Name");
+
+        session.save(a1);
+        session.getTransaction().commit();
+
+        session.close();
+
+
     }
 }
